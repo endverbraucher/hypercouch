ec30c23722b03a34d1a8eace2a52e768e1e63c4a
Thomas Schrader <slogmen@gmail.com>
2012-09-06 00:25:05 +0200

    CodeMirror intégration fixes

--- app/assets/javascripts/backend/application.js
+++ app/assets/javascripts/backend/application.js
@@ -5,30 +5,6 @@
 // = require twitter/bootstrap/bootstrap-tab
 // = require twitter/bootstrap/bootstrap-button
 
-// = require codemirror
-// = require codemirror/modes/markdown
-
 $('.delete_button').live ('ajax:success', function() {
 	$(this).closest('.item').fadeOut('fast');
 });
-
-jQuery(function($) {
-	$('div.btn-group').each(function(){
-	    var group   = $(this);
-	    var form    = group.parents('form').eq(0);
-	    var name    = group.attr('data-toggle-name');
-	    var hidden  = $('input[name="' + name + '"]', form);
-	    $('button', group).each(function(){
-	      var button = $(this);
-	      button.live('click', function(){
-	          hidden.val($(this).val());
-	      });
-	      if(button.val() == hidden.val()) {
-	        button.addClass('active');
-	      }
-	    });
-	  });
-});
-
-
-var myCodeMirror = CodeMirror.fromTextArea(post_mdown, {lineWrapping: true});
