9924e2d3b7fa502af2ff314403692b615562217b
Thomas Schrader <slogmen@gmail.com>
2012-09-06 00:33:02 +0200

    fixing.

--- app/assets/javascripts/backend/application.js
+++ app/assets/javascripts/backend/application.js
@@ -4,7 +4,30 @@
 // = require twitter/bootstrap/bootstrap-alert
 // = require twitter/bootstrap/bootstrap-tab
 // = require twitter/bootstrap/bootstrap-button
+// = require codemirror
+// = require codemirror/modes/markdown
+// = require codemirror/modes/gfm
 
 $('.delete_button').live ('ajax:success', function() {
 	$(this).closest('.item').fadeOut('fast');
 });
+
+jQuery(function($) {
+	$('div.btn-group').each(function(){
+	    var group   = $(this);
+	    var form    = group.parents('form').eq(0);
+	    var name    = group.attr('data-toggle-name');
+	    var hidden  = $('input[name="' + name + '"]', form);
+	    $('button', group).each(function(){
+	      var button = $(this);
+	      button.live('click', function(){
+	          hidden.val($(this).val());
+	      });
+	      if(button.val() == hidden.val()) {
+	        button.addClass('active');
+	      }
+	    });
+	  });
+});
+
+var myCodeMirror = CodeMirror.fromTextArea(post_mdown, {lineWrapping: true});
