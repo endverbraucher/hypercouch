9cdf2769d8e8447792e6b47cd5f510fefe2eeb4a
Thomas Schrader <slogmen@gmail.com>
2013-02-12 21:57:29 +0100

    cleaned up the mess.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,28 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = CouchPotato.database.view(Post.published(limit: 11, descending: true))["rows"]
+    @posts = CouchPotato.database.view(
+      Post.published(limit: 11, descending: true))["rows"]
     @pub_date = @posts.first["value"].pubdate;
+    @more_posts = CouchPotato.database.view(
+      Post.archive(stale: 'ok', limit: 11, descending: true,
+      :startkey => @posts.last["key"] ))["rows"]
 
-    stale? @posts.first["value"], public: true do
-      respond_to do |format|
-        format.html # index.html.erb
-        format.atom
-        format.json { render json: @posts }
-      end
+    respond_to do |format|
+      format.html # index.html.erb
+      format.atom
+      format.json { render json: @posts }
     end
+
   end
 
   def more
-    @posts = CouchPotato.database.view(Post.published(stale: 'ok', limit: 11, descending: true, :startkey => params[:startkey].to_i))["rows"]
+    @posts = CouchPotato.database.view(
+    Post.published(stale: 'ok', limit: 11, descending: true,
+      :startkey => params[:startkey].to_i))["rows"]
 
-    stale? @posts.first["value"], public: true do
-      respond_to do |format|
-        format.js { render :content_type => 'text/javascript' }
-      end
+    respond_to do |format|
+      format.js { render :content_type => 'text/javascript' }
     end
   end
 
@@ -27,10 +30,8 @@ class PostsController < ApplicationController
     @post = CouchPotato.database.load_document(params[:slug])
     @pub_date = @post.pubdate;
 
-    stale? @post, public: true do
-      respond_to do |format|
-        format.html # show.html.erb
-      end
+    respond_to do |format|
+      format.html # show.html.erb
     end
   end
 
@@ -38,10 +39,8 @@ class PostsController < ApplicationController
     headers['Content-Type'] = 'application/xml'
     @posts = CouchPotato.database.view(Post.published())["rows"]
 
-    stale? @posts.last["value"], public:true do
-      respond_to do |format|
-        format.xml { @posts }
-      end
+    respond_to do |format|
+      format.xml { @posts }
     end
   end
 
