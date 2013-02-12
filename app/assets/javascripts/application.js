9196fb0c060281de4c4ce0a82cb9cfa33d0a32ed
Thomas Schrader <slogmen@gmail.com>
2013-02-12 21:58:40 +0100

    reworked layout.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,18 @@
 //= require jquery
 //= require jquery_ujs
+//= require jquery_rotate
+
+$(document).ready(function(){
+
+  $(".remark-refresh").click(function () {
+
+    $("img", this).rotate({
+      duration: 1500,
+      angle: 0,
+      animateTo:360,
+      callback: function() {
+        $(".m-hero-remark").hide().html("Perlen vor die Säue").fadeIn("slow");
+      }
+    });
+  })
+});
