88b59d78bba22bc3d651653c2e11bb33d8c98398
Thomas Schrader <slogmen@gmail.com>
2013-03-01 11:14:46 +0100

    added stale back to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,12 @@ class PostsController < ApplicationController
     @posts = CouchPotato.database.view(Post.published(limit: 15, descending: true))
     @pub_date = [@posts.first.published_at.to_datetime.rfc3339, @posts.first.pubdate];
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.atom
-      format.json { render json: @posts }
+    stale? @posts.first, public: true do
+      respond_to do |format|
+        format.html # index.html.erb
+        format.atom
+        format.json { render json: @posts }
+      end
     end
 
   end
@@ -18,14 +20,15 @@ class PostsController < ApplicationController
     @archive = Hash[
       posts.group_by(&:pub_year).map{|year, posts|
         [year, posts.group_by{|post| post.pub_month}]
-      }
-    ]
+    }]
 
     @pub_date = [posts.first.published_at.to_datetime.rfc3339,
       posts.first.pubdate];
 
-      respond_to do |format|
-        format.html # show.html.erb
+      stale? @posts.first["value"], public: true do
+        respond_to do |format|
+          format.html # show.html.erb
+        end
       end
     end
 
@@ -33,8 +36,10 @@ class PostsController < ApplicationController
       @post = CouchPotato.database.load_document(params[:slug])
       @pub_date = [@post.published_at.to_datetime.rfc3339, @post.pubdate];
 
-      respond_to do |format|
-        format.html # show.html.erb
+      stale? @post, public: true do
+        respond_to do |format|
+          format.html # show.html.erb
+        end
       end
     end
 
@@ -42,8 +47,10 @@ class PostsController < ApplicationController
       headers['Content-Type'] = 'application/xml'
       @posts = CouchPotato.database.view(Post.published())
 
-      respond_to do |format|
-        format.xml { @posts }
+      stale? @post, public: true do
+        respond_to do |format|
+          format.xml { @posts }
+        end
       end
     end
 
