bb7c3715d0547681c27d96825a27bd6b5cd764c4
Thomas Schrader <slogmen@gmail.com>
2013-03-04 19:57:16 +0100

    secured the secret token. ;-)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,11 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Hypercouch::Application.config.secret_token = 'c469862e36bd1e35b0ae37eac39473cc367f0556b7bdcccb242db06a1df63010984c5e367bb815e391ec1876e513df642d134cf9406c9a1d4a374a14d050edbc'
+
+secret = ENV['slog_secret']
+
+if secret.length < 30
+  raise 'Secret token can not be loaded!'
+else
+  Hypercouch::Application.config.secret_token = secret
+end
