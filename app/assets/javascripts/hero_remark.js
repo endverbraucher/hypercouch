6adfcfc0c4a191e65ebeae4a64f1eb88bd849d05
Thomas Schrader <slogmen@gmail.com>
2013-03-04 10:15:52 +0100

    finished js for statements/remarks.

--- app/assets/javascripts/hero_remark.js
+++ app/assets/javascripts/hero_remark.js
@@ -1,11 +1,29 @@
+var claims = [
+  "bin neugierig.",
+  "löse gern Probleme.",
+  "liebe Dresden.",
+  "studierte einst in Trier.",
+  "wohne jetzt Köln.",
+  "liebe gut benutzbare Dinge.",
+  "beobachte Menschen.",
+  "beobachte Maschinen.",
+  "mach mal eine Pause.",
+  ];
+
 $(document).ready(function(){
   $(".remark-refresh").click(function () {
+    event.preventDefault();
+
     $("svg", this).rotate({
-      duration: 1500,
+      duration: 1100,
       angle: 0,
       animateTo:360,
       callback: function() {
-        $(".m-hero-remark").hide().html("Perlen vor die Säue").fadeIn();
+        $(".m-hero-remark").hide().html(claims.shift()).fadeIn();
+
+        if (claims.length == 0) {
+          $(".remark-refresh").remove();
+        };
       }
     });
   })
