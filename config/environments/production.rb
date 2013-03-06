a910d428d72c77bdffcc43d39f5a7f2a3bef8f38
Thomas Schrader <slogmen@gmail.com>
2013-03-06 14:07:30 +0100

    change config for production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,9 @@ Hypercouch::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
+  # Faster asset precompilation. Required by Heroku!
+  config.assets.initialize_on_precompile = false
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
