4dc996d1ee1a840bb9097d967a267300ce294a5c
Thomas Schrader <slogmen@gmail.com>
2013-03-06 13:47:16 +0100

    added fallback for secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -7,8 +7,8 @@
 
 secret = ENV['slog_secret']
 
-if secret.length < 30
-  raise 'Secret token can not be loaded!'
-else
-  Hypercouch::Application.config.secret_token = secret
+if secret.nil?
+  secret = 'c4e87a0b812a57b074de00f0cab7f11e687bc336da3d7d80859cfbbd81da50a177c6318a04e28a2d74020da7321ae4dd5e318081fd957d9984dd7800d01a4427'
 end
+
+Hypercouch::Application.config.secret_token = secret
