2683b37d6ebc294157b1c87b5b10eaac9ce69056
Thomas Schrader <slogmen@gmail.com>
2013-03-07 11:53:17 +0100

    added email obfuscation.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,13 @@
 //= require jquery.turbolinks
 //= require jquery_rotate
 //= require hero_remark
+
+String.prototype.rot13 = function(){
+    return this.replace(/[a-zA-Z]/g, function(c){
+        return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+    });
+};
+
+$(document).ready(function(){
+  $('.twitter-link').after('<li><a href="mailto:'+ 'unyyb@fybt.vb'.rot13() +'?subject=Guten Tag!">sag Hallo!</a></li>');
+});
