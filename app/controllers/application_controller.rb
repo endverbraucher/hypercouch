79d76ff6e56ff1a3d14889f4895939c709ba558a
Thomas Schrader <slogmen@gmail.com>
2013-03-08 23:45:02 +0100

    removed authorize for backend.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  def authorize
-    if session[:user_id].nil?
-      redirect_to login_url, :notice => "Please log in!"    
-    end
-  end
-    
 end
